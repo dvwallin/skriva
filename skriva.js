2f487c1bd751b7b2cb7c99084b7ccc1e6e45e973
David Satime Wallin <david@dwall.in>
2020-11-19 19:58:13 +0100

    new sounds and a new word
    Signed-off-by: David Satime Wallin <david@dwall.in>

--- skriva.js
+++ skriva.js
@@ -26,6 +26,7 @@ function init() {
 	}
 
     $('#myword').html( newWord );
+    $('#cc-0').addClass("circle");
     $('#mywordimg').html('<img src="imgs/'+word+'.jpg" />');
 }
 
@@ -41,9 +42,15 @@ function reset() {
 }
 
 function generateWord() {
-    var words = new Array("apa", "elefant", "fågel", "hund", "katt", "lejon", "råtta", "snigel", "spindel", "tiger", "uggla", "penna", "ko", "sol", "måne", "dator", "bil", "buss", "äpple");
+    var words = new Array("apa", "elefant", "fågel", "hund", "katt", "lejon", "råtta", "snigel", "spindel", "tiger", "uggla", "penna", "ko", "sol", "måne", "dator", "bil", "buss", "äpple", "snögubbe");
     randno = words[Math.floor( Math.random() * words.length )];
     return randno;
+}
+function resetCircle(word) {
+    for (var x = 0; x < word.length; x++ )
+    {
+        $('#cc-' + x).removeClass("circle");
+    }
 }
 // Yes!
 $('#myinput').on("input", function() {
@@ -52,6 +59,9 @@ $('#myinput').on("input", function() {
     var reg = new RegExp("/^"+myInput+"/i");
     var pos = myInput.length - 1;
     var len = myInput.length;
+    var nextID = '#cc-' + myInput.length;
+    resetCircle(myWord);
+    $(nextID).addClass("circle");
     var letterInWord = myWord.substring(0, len);
     var letterInInput = myInput.substring(0, len);
     var audioElement = document.createElement('audio');
@@ -78,18 +88,22 @@ $('#myinput').on("input", function() {
 		setTimeout(
 			function()
 			{
-				audioElement.setAttribute('src', 'yay.wav');
-				$('#inputArea').addClass('hidden');
-				$('div#success--box').removeClass('hidden');
-				$('h1#success--box--word').text(myWord);
-				$('div#success--box--img').html('<img src="imgs/'+myWord.toLowerCase()+'.jpg" />');
-				$('body').addClass('allGreen');
-				audioElement.play();
-				$.wait(3000).then(function() {
-				    console.log($.session.get('stars'));
-				    reset();
-				    init();
-				});
+                $('#inputArea').addClass('hidden');
+                $('div#success--box').removeClass('hidden');
+                $('h1#success--box--word').text(myWord);
+                $('div#success--box--img').html('<img src="imgs/'+myWord.toLowerCase()+'.jpg" />');
+                $('body').addClass('allGreen');
+                audioElement.setAttribute('src', 'sounds/'+myWord.toLowerCase()+'.mp3');
+                audioElement.play();
+                $.wait(1000).then(function() {
+                    audioElement.setAttribute('src', 'yay.wav');
+                    audioElement.play();
+                    $.wait(3000).then(function() {
+                        console.log($.session.get('stars'));
+                        reset();
+                        init();
+                    });
+                });
 			}, 850);
     }
 });
